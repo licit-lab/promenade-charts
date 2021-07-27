768d25e69f55e4c6bfb300a73c98d9d78b678a38
woland7 <antonio.deiasio@gmail.com>
2021-07-27 12:42:53 +0200

    update

--- menu.sh
+++ menu.sh
@@ -4,27 +4,25 @@ oc login master.licit.local:8443 --username=licit --password=licit
 
 oc project promenade
 
+spark() {
+  (
+    cd spark-operator/spark || exit; oc apply -f pod.yaml
+  )
+}
+
 artemis() {
-  ##Creating artemis here
   (cd activemq-artemis/activemq-artemis || exit
-  #
   helm delete artemis
-  #
   oc delete pvc data-artemis-activemq-artemis-master-0
   oc delete pvc artemis-activemq-artemis
-
   helm install artemis .)
 }
 
 mongo() {
   (cd mongodb || exit
-  #
-  helm delete mongodb-replicaset
-  #
-  oc delete pvc datadir-mongodb-replicaset-0
-  oc delete pvc datadir-mongodb-replicaset-1
-
-  helm install mongodb-replicaset .)
+  helm delete mongodb
+  oc delete pvc mongodb
+  helm install mongodb .)
 }
 
 incorrect_selection() {
@@ -36,7 +34,41 @@ neo4j(){
 }
 
 kafka(){
-  echo "to be done"
+  helm delete kafka
+  helm delete kafka-connect-artemis
+  helm delete connect-mongodb-sink-ingestion
+  helm delete connect-mongodb-sink-processing
+  oc delete pvc datadir-kafka-cp-zookeeper-2
+  oc delete pvc datadir-kafka-cp-zookeeper-1
+  oc delete pvc datadir-kafka-cp-zookeeper-0
+  oc delete pvc datadir-0-kafka-cp-kafka-2
+  oc delete pvc datadir-0-kafka-cp-kafka-1
+  oc delete pvc datadir-0-kafka-cp-kafka-0
+  oc delete pvc datalogdir-kafka-cp-zookeeper-0
+  oc delete pvc datalogdir-kafka-cp-zookeeper-1
+  oc delete pvc datalogdir-kafka-cp-zookeeper-2
+
+  (
+    cd cp-kafka-helm-charts || exit; helm install kafka .
+
+    (cd charts/cp-kafka-connect-artemis || exit; sleep 100; helm install kafka-connect-artemis .)
+
+    (cd charts/connect-mongodb-sink-ingestion || exit; helm install connect-mongodb-sink-ingestion .)
+
+    (cd charts/connect-mongodb-sink-processing || exit; helm install connect-mongodb-sink-processing .)
+  )
+
+  sleep 90
+
+  (cd config || exit;
+  echo -e "Creating Artemis Source Connector"
+  curl -s -X POST -H 'Content-Type: application/json' --data @artemis-source.json  http://connect-artemis-promenade.router.default.svc.cluster.local/connectors
+  sleep 10
+  echo -e "Creating MongoDB Sink Ingestion Connector"
+  curl -s -X POST -H 'Content-Type: application/json' --data @mongodb-sink-ingestion.json  http://connect-mongo-sink-ingestion.router.default.svc.cluster.local/connectors
+  sleep 10
+  echo -e "Creating MongoDB Sink Processing Connector"
+  curl -s -X POST -H 'Content-Type: application/json' --data @mongodb-sink-processing.json  http://connect-mongo-sink-processing.router.default.svc.cluster.local/connectors)
 }
 
 execute_all(){
@@ -50,7 +82,8 @@ until [ "$selection" = "0" ]; do
   echo "2  -  Mongo"
   echo "3  -  Kafka"
   echo "4  -  Neo4j"
-  echo "5  -  Execute all"
+  echo "5 -   Spark"
+  echo "6  -  Execute all"
   echo "0  -  Exit"
   echo ""
   echo -n "Enter selection: "
@@ -60,7 +93,8 @@ until [ "$selection" = "0" ]; do
     2 ) mongo ;;
     3 ) kafka ;;
     4 ) neo4j ;;
-    5 ) execute_all ;;
+    5 ) spark ;;
+    6 ) execute_all ;;
     0 ) exit ;;
     * ) incorrect_selection ;;
   esac
--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
 All the charts
+
+### Launch a new connector instance
+```
+cd /etc; curl -s -X POST -H 'Content-Type: application/json' --data @artemis-source.json http://localhost:8083/connectors
+cd /etc/neo4j; curl -s -X POST -H 'Content-Type: application/json' --data @neo4j-sink.json http://localhost:8083/connectors
+cd /etc; curl -s -X POST -H 'Content-Type: application/json' --data @mongodb-sink.json http://localhost:8083/connectors
+curl -s -X DELETE http://localhost:8083/connectors/neo4j-sink
+curl -s -X DELETE  http://connect-mongo-sink-ingestion.router.default.svc.cluster.local/connectors/mongodb-sink
+curl -s -X DELETE http://localhost:8083/connectors/artemis-source
+curl -s -X DELETE http://connect-artemis-promenade.router.default.svc.cluster.local/connectors/artemis-source
+```
